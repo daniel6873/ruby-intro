351a0bbda193dfef0e59aac0c5fc12e1ec24ea97
Daniel Mobayyen <daniel.mobayyen@kellogg.northwestern.edu>
2023-04-20 00:29:52 +0000

    Problem 2 completed

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,12 +3,34 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+puts true
+puts false
+puts 3 < 2
+puts 3==2
+
 
 # Boolean Expressions
 
 # If Conditional Logic
 
+if 3 > 2
+    puts "maths works"
+end 
+
 # If/Else Conditional Logic
+
+home_team_score = 1
+away_team_score = 2
+
+if home_team_score > away_team_score
+    puts "Home team wins!"
+elsif
+    home_team_score==away_team_score
+    puts "tied game"
+else 
+    home_team_score < away_team_score
+    puts "Away team wins!"
+end
 
 # Elsif Conditional Logic
 
